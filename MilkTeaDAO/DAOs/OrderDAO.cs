ebca4361a056554f007199758ac491ed0bdbc08b
{Doanh} <{nhatdoanhnguyen@gmail.com}>
2024-05-29 22:54:11 +0700

    Crud api order

---
+++ MilkTeaDAO/DAOs/OrderDAO.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkTeaBusinessObject.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaDAO.DAOs
+{
+    public class OrderDAO
+    {
+       
+            private readonly MilkTeaDeliveryDBContext _context;
+
+            public OrderDAO()
+            {
+                _context = new MilkTeaDeliveryDBContext();
+            }
+
+            public List<Order> GetList()
+            {
+                try
+                {
+                    return _context.Orders.Include(o => o.User).Include(o => o.OrderDetails)
+                                          .ThenInclude(od => od.Tea)
+                                          .ToList();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            public Order Get(int id)
+            {
+                try
+                {
+                    var order = _context.Orders.Include(o => o.User).Include(o => o.OrderDetails)
+                                               .ThenInclude(od => od.Tea)
+                                               .SingleOrDefault(o => o.OrderID == id);
+                    return order;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            public void Add(Order order)
+            {
+                try
+                {
+                order.OrderID = 0;
+                    _context.Orders.Add(order);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            public void Update(Order order)
+            {
+                try
+                {
+                    var existingOrder = _context.Orders.SingleOrDefault(o => o.OrderID == order.OrderID);
+                    if (existingOrder != null)
+                    {
+                        _context.Entry(existingOrder).CurrentValues.SetValues(order);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("Order not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            public void Delete(int id)
+            {
+                try
+                {
+                    var order = _context.Orders.SingleOrDefault(o => o.OrderID == id);
+                    if (order != null)
+                    {
+                        _context.Orders.Remove(order);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("Order not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+}
---
+++ MilkTeaServices/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using MilkTeaBusinessObject.BusinessObject;
+using MilkTeaRepository.IRepo;
+using MilkTeaServices.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaServices.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepo _OrderRepo;
+        public OrderService(IOrderRepo OrderRepo)
+        {
+            _OrderRepo = OrderRepo;
+        }
+        public List<Order> getList() => _OrderRepo.getList();
+        public Order get(int id) => _OrderRepo.get(id);
+        public void delete(int id) => _OrderRepo.delete(id);
+        public void update(Order order) => _OrderRepo.update(order);
+        public void add(Order order) => _OrderRepo.add(order);
+    }
+}
