2c8982db1d4d25dcdd3fed2675722dbf47491a90
Anlq <lequangan2404@gmail.com>
2024-07-04 16:17:36 +0700

    fix merge

--- MilkTeaDAO/DAOs/OrderDAO.cs
+++ MilkTeaDAO/DAOs/OrderDAO.cs
@@ -29,82 +29,13 @@ namespace MilkTeaDAO.DAOs
                                           .ThenInclude(od => od.Tea)
                                           .ToList();
                 }
-                catch (Exception ex)
+                catch (Exception e)
                 {
-                    throw new Exception(ex.Message);
+                    throw new Exception(e.Message);
                 }
             }
 
-            public Order Get(int id)
-            {
-                try
-                {
-                    var order = _context.Orders.Include(o => o.OrderDetails)
-                                               .ThenInclude(od => od.Tea)
-                                               .SingleOrDefault(o => o.OrderID == id);
-                    return order;
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
-
-            public Order Add(Order order)
-            {
-                try
-                {
-                    _context.Orders.Add(order);
-                    _context.SaveChanges();
-                return _context.Orders.Take(1).OrderByDescending(o => o.StartDate).FirstOrDefault();
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
-
-            public void Update(Order order)
-            {
-                try
-                {
-                    var existingOrder = _context.Orders.SingleOrDefault(o => o.OrderID == order.OrderID);
-                    if (existingOrder != null)
-                    {
-                        _context.Entry(existingOrder).CurrentValues.SetValues(order);
-                        _context.SaveChanges();
-                    }
-                    else
-                    {
-                        throw new Exception("Order not found");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
-
-            public void Delete(int id)
-            {
-                try
-                {
-                    var order = _context.Orders.SingleOrDefault(o => o.OrderID == id);
-                    if (order != null)
-                    {
-                        _context.Orders.Remove(order);
-                        _context.SaveChanges();
-                    }
-                    else
-                    {
-                        throw new Exception("Order not found");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
+           
         }
 
         public Order Get(int id)
