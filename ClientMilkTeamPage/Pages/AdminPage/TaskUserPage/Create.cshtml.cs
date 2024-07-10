f1a741308d15043a2477f743455e9cafae1d2ed0
Anlq <lequangan2404@gmail.com>
2024-07-10 11:26:25 +0700

    update taskUI, fix and order hist, list UI  and sort newest

--- ClientMilkTeamPage/Pages/AdminPage/TaskUserPage/Create.cshtml.cs
+++ ClientMilkTeamPage/Pages/AdminPage/TaskUserPage/Create.cshtml.cs
@@ -11,27 +11,27 @@ namespace ClientMilkTeamPage.Pages.AdminPage.TaskUserPage
         private readonly HttpClient client = null!;
         private string ApiUrl = "";
 
-
         public CreateModel()
         {
             client = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             client.DefaultRequestHeaders.Accept.Add(contentType);
             ApiUrl = "https://localhost:7112/odata/TaskUser";
-
         }
 
         [BindProperty]
         public TaskUserCreateDTO TaskUser { get; set; } = default!;
 
-
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
             try
             {
-
                 string strData = JsonSerializer.Serialize(TaskUser);
                 var contentData = new StringContent(strData, System.Text.Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await client.PostAsync(ApiUrl, contentData);
@@ -40,15 +40,17 @@ namespace ClientMilkTeamPage.Pages.AdminPage.TaskUserPage
                     ViewData["Message"] = "Add New Task successfully";
                     return RedirectToPage("./Index");
                 }
+                else
+                {
+                    ViewData["ErrorMessage"] = "Error while adding new task";
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                ViewData["ErrorMessage"] = "Fail To Call API";
-
-                return Page();
+                ViewData["ErrorMessage"] = $"Fail To Call API: {ex.Message}";
             }
 
-            return RedirectToPage("./Index");
+            return Page();
         }
     }
 }
