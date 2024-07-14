d6f30266538129a0d334805715b27a205707ec5d
dangquangtran <quangtran2002qn@gmail.com>
2024-07-14 21:52:50 +0700

    material valid

--- ClientMilkTeamPage/DTO/MaterialDTO/MaterialCreateDTO.cs
+++ ClientMilkTeamPage/DTO/MaterialDTO/MaterialCreateDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace ClientMilkTeamPage.DTO.MaterialDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientMilkTeamPage.DTO.MaterialDTO
 {
     public class MaterialCreateDTO
     {
         public string MaterialName { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity phải lớn hơn 0.")]
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public bool Status { get; set; }
--- ClientMilkTeamPage/DTO/MaterialDTO/MaterialUpdateDTO.cs
+++ ClientMilkTeamPage/DTO/MaterialDTO/MaterialUpdateDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace ClientMilkTeamPage.DTO.MaterialDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientMilkTeamPage.DTO.MaterialDTO
 {
     public class DetailMateriaUpdateDTO
     {
         public int MaterialID { get; set; }
         public string MaterialName { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity phải lớn hơn 0.")]
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public bool Status { get; set; }
