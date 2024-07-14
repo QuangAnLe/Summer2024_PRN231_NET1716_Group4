1c16a999e9e729f7c50051120eeb6f1ba4d114cc
dangquangtran <quangtran2002qn@gmail.com>
2024-07-14 21:56:34 +0700

    tea valid

--- ClientMilkTeamPage/DTO/TeaDTO/TeaCreateDTO.cs
+++ ClientMilkTeamPage/DTO/TeaDTO/TeaCreateDTO.cs
@@ -1,9 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace ClientMilkTeamPage.DTO.TeaDTO
 {
     public class TeaCreateDTO
     {
         public string TeaName { get; set; }
         public int Estimation { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price phải lớn hơn 0.")]
         public decimal Price { get; set; }
         public string TeaDescription { get; set; }
         public bool Status { get; set; }
--- ClientMilkTeamPage/DTO/TeaDTO/TeaUpdateDTO.cs
+++ ClientMilkTeamPage/DTO/TeaDTO/TeaUpdateDTO.cs
@@ -1,10 +1,13 @@
-﻿namespace ClientMilkTeamPage.DTO.TeaDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientMilkTeamPage.DTO.TeaDTO
 {
     public class TeaUpdateDTO
     {
         public int TeaID { get; set; }
         public string TeaName { get; set; }
         public int Estimation { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price phải lớn hơn 0.")]
         public decimal Price { get; set; }
         public string TeaDescription { get; set; }
         public bool Status { get; set; }
