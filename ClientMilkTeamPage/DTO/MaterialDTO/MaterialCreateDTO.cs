903139643c2c313b0d7c12bdfb84b0e5abbbcdff
dangquangtran <quangtran2002qn@gmail.com>
2024-07-14 21:55:41 +0700

    material valid

--- ClientMilkTeamPage/DTO/MaterialDTO/MaterialCreateDTO.cs
+++ ClientMilkTeamPage/DTO/MaterialDTO/MaterialCreateDTO.cs
@@ -7,6 +7,7 @@ namespace ClientMilkTeamPage.DTO.MaterialDTO
         public string MaterialName { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Quantity phải lớn hơn 0.")]
         public int Quantity { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price phải lớn hơn 0.")]
         public decimal Price { get; set; }
         public bool Status { get; set; }
     }
