9d10b919b51231b708c21b891d4be4b1e7da8913
dangquangtran <quangtran2002qn@gmail.com>
2024-07-14 22:05:27 +0700

    orderdetail valid

--- ClientMilkTeamPage/DTO/OrderDetail.cs
+++ ClientMilkTeamPage/DTO/OrderDetail.cs
@@ -1,9 +1,13 @@
-﻿namespace ClientMilkTeamPage.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientMilkTeamPage.DTO
 {
     public class OrderDetail
     {
         public int OrderDetailID { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "TotalPrice phải lớn hơn 0.")]
         public double TotalPrice { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity phải lớn hơn 0.")]
         public int Quantity { get; set; }
         public string CostsIncurred { get; set; }
         public int OrderID { get; set; }
