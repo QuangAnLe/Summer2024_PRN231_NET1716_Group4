73e8975f5ebab9b042c3151314833d11c899fcef
dangquangtran <quangtran2002qn@gmail.com>
2024-07-14 22:01:51 +0700

    comment valid

--- ClientMilkTeamPage/DTO/CommentDTO/CommentCreateDTO.cs
+++ ClientMilkTeamPage/DTO/CommentDTO/CommentCreateDTO.cs
@@ -1,10 +1,13 @@
-﻿namespace ClientMilkTeamPage.DTO.CommentDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientMilkTeamPage.DTO.CommentDTO
 {
     public class CommentCreateDTO
     {
         public int CommentID { get; set; }
         public DateTime CommentDate { get; set; }
         public string Content { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Rating phải lớn hơn 0.")]
         public int Rating { get; set; }
         public int TeaID { get; set; }
         public int UserID { get; set; }
