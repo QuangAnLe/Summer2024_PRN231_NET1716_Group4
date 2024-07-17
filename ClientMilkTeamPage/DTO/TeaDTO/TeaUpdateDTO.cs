bdc296312b4361f877169ba283c9d578a408da9a
Hoang Nam <nampham13072@gmail.com>
2024-07-17 23:47:18 +0700

    validation estamination tea với thêm trong giao diện homepage

--- ClientMilkTeamPage/DTO/TeaDTO/TeaUpdateDTO.cs
+++ ClientMilkTeamPage/DTO/TeaDTO/TeaUpdateDTO.cs
@@ -6,7 +6,8 @@ namespace ClientMilkTeamPage.DTO.TeaDTO
     {
         public int TeaID { get; set; }
         public string TeaName { get; set; }
-        public int Estimation { get; set; }
+		[Range(0, double.MaxValue, ErrorMessage = "Estimation >= 0.")]
+		public int Estimation { get; set; }
         [Range(0.01, double.MaxValue, ErrorMessage = "Price phải lớn hơn 0.")]
         public decimal Price { get; set; }
         public string TeaDescription { get; set; }
